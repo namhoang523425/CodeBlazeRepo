5c7aba3ce6e20b7a720973efdd36127fa3e4494e
namhoang523425 <mcollard59nam1311@outlook.com>
2024-05-08 11:40:28 +0700

    hello

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-function productExceptSelf(nums) {
-  const result = [];
-  let product = 1;
-  for (let i = 0; i < nums.length; i++) {
-    result[i] = product;
-    product *= nums[i];
-  }
-  product = 1;
-  for (let i = nums.length - 1; i >= 0; i--) {
-    result[i] *= product;
-    product *= nums[i];
-  }
-  return result;
+function getMax(arr) {
+  return Math.max(...arr);
 }
