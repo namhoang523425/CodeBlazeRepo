f2b46e71b6ad87d934bf1f6b37ac5023fe63baa9
namhoang523425 <mcollard59nam1311@outlook.com>
2024-05-10 13:09:38 +0700

    update sdk project

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function getMin(arr) {
-  return Math.min(...arr);
+function removeKdigits(num, k) {
+  const stack = [];
+  for (const digit of num) {
+    while (k > 0 && stack.length && stack[stack.length - 1] > digit) {
+      stack.pop();
+      k--;
+    }
+    stack.push(digit);
+  }
+  while (k > 0) {
+    stack.pop();
+    k--;
+  }
+  let result = "";
+  let leadingZero = true;
+  for (const digit of stack) {
+    if (leadingZero && digit === "0") continue;
+    leadingZero = false;
+    result += digit;
+  }
+  return result || "0";
 }
