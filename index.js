3e9f5f12614483789e91895fcc1bc6ddc8b5d16e
namhoang523425 <mcollard59nam1311@outlook.com>
2024-05-16 00:17:13 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-function linearSearch(arr, target) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === target) return i;
+function isSymmetric(root) {
+  if (!root) return true;
+  return isMirror(root.left, root.right);
+  function isMirror(left, right) {
+    if (!left && !right) return true;
+    if (!left || !right || left.val !== right.val) return false;
+    return isMirror(left.left, right.right) && isMirror(left.right, right.left);
   }
-  return -1;
 }
