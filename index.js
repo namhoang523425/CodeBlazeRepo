cf682aa9c6bdb7aed009ea23977bc71e67719d04
namhoang523425 <mcollard59nam1311@outlook.com>
2024-05-15 10:28:55 +0700

    add more features

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
-function sortedListToBST(head) {
-  if (!head) return null;
-  if (!head.next) return new TreeNode(head.val);
-  let slow = head;
-  let fast = head;
-  let prev = null;
-  while (fast && fast.next) {
-    prev = slow;
-    slow = slow.next;
-    fast = fast.next.next;
+function linearSearch(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === target) return i;
   }
-  const root = new TreeNode(slow.val);
-  prev.next = null;
-  root.left = sortedListToBST(head);
-  root.right = sortedListToBST(slow.next);
-  return root;
+  return -1;
 }
